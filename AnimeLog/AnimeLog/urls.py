253126ed622d7c9af001ab0dd18bcfc9c820606a
saltgyro <salt.gyro@gmail.com>
2024-12-22 18:17:47 +0900

    git初期登録

---
+++ AnimeLog/AnimeLog/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path,include
+from . import settings
+from django.contrib.staticfiles.urls import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('anime_tracker/',include('anime_tracker.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
---
+++ AnimeLog/anime_tracker/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AnimeTrackerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'anime_tracker'
