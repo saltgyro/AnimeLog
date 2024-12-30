a03ff4ac75b66711ed6db7da8f8f704411526a37
saltgyro <salt.gyro@gmail.com>
2024-12-31 08:16:20 +0900

    トップページ変更

--- AnimeLog/AnimeLog/urls.py
+++ AnimeLog/AnimeLog/urls.py
@@ -5,7 +5,8 @@ from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('anime_tracker/',include('anime_tracker.urls')),
+    # path('anime_tracker/',include('anime_tracker.urls')),
+    path('', include('anime_tracker.urls')),  
 ]
 
 if settings.DEBUG:
